2041cad680456373b1f55992f9c684a06c4d7632
Felipe <jaimenawath@puntop.cl>
2020-06-08 04:49:04 -0300

    sube

---
+++ public/precache-manifest.e693b64ace327a47550a837403d5d1f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3a5bbecb49a5d03666202ffecdb787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5c943cca35e84a4313",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "85a3eb023a6cde25d61c",
+    "url": "/static/css/main.3bee8719.chunk.css"
+  },
+  {
+    "revision": "2c5c943cca35e84a4313",
+    "url": "/static/js/2.acbad096.chunk.js"
+  },
+  {
+    "revision": "b7b068e7a9ff7c79ac98ce106091f1d8",
+    "url": "/static/js/2.acbad096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a3eb023a6cde25d61c",
+    "url": "/static/js/main.6c345f96.chunk.js"
+  },
+  {
+    "revision": "21de9c42ff9843d813cd",
+    "url": "/static/js/runtime-main.de186966.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
